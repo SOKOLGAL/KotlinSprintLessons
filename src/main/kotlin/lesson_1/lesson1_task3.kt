068a7eb378c282f005f71466cd6da2dab630dbf7
Sokolova <sokolovagb@gmail.com>
2025-04-05 21:51:14 +0300

    KS-1-3 space

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -4,9 +4,9 @@ fun main() {
     val year = 1961
     var hour = 9
     var minute = 7
-//  println ("$year")
-//  println ("$hour")
-//  println ("$minute")
+//  println("$year")
+//  println("$hour")
+//  println("$minute")
     hour = 10
     minute = 55
     println("$hour:$minute")
