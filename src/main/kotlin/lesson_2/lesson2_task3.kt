d856e3261cea5cf602356b68000b94144c45dfd7
Sokolova <sokolovagb@gmail.com>
2025-04-06 21:25:54 +0300

    KS-2-3 arrival time

---
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_2
+
+fun main() {
+    val departureHours = 9
+    val departureMinutes = 39
+    val travelTimeMinutes = 457
+    val MINUTES_IN_HOUR = 60
+    val travelTimeHours = travelTimeMinutes / MINUTES_IN_HOUR
+    val travelMinutes = travelTimeMinutes % MINUTES_IN_HOUR
+    val arrivalTime = departureHours + travelTimeHours
+    val arrivalMinutes = departureMinutes + travelMinutes
+    val hour = arrivalMinutes / MINUTES_IN_HOUR
+//    println("%02d:%02d".format(departureHours, departureMinutes))
+    println("%02d:%02d".format(arrivalTime + hour, arrivalMinutes - MINUTES_IN_HOUR))
+}
