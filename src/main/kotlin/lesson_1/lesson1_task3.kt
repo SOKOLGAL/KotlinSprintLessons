f0787d034f0082200878b5e109d1a9b9c751ef2a
Sokolova <sokolovagb@gmail.com>
2025-04-06 19:42:24 +0300

    KS-1-3 hours minutes

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -2,12 +2,12 @@ package org.example.lesson_1
 
 fun main() {
     val year = 1961
-    var hour = 9
-    var minute = 7
+    var hour = "%02d".format(9)
+    var minute = "%02d".format(7)
 //  println("$year")
 //  println("$hour")
 //  println("$minute")
-    hour = 10
-    minute = 55
+     hour = 10.toString()
+     minute = 55.toString()
     println("$hour:$minute")
 }
