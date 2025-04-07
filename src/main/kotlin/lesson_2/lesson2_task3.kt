49609c76e047b7f1e6b8ba991583886a14b46abc
Sokolova <sokolovagb@gmail.com>
2025-04-07 19:51:35 +0300

    KS-2-3 design

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -5,7 +5,6 @@ fun main() {
     val departureHours = 9
     val departureMinutes = 39
     val travelTimeMinutes = 457
-    val MINUTES_IN_HOUR = 60
 
     val travelTimeHours = travelTimeMinutes / MINUTES_IN_HOUR
     val travelMinutes = travelTimeMinutes % MINUTES_IN_HOUR
@@ -16,3 +15,5 @@ fun main() {
     println("%02d:%02d".format(arrivalTime + hour, arrivalMinutes - MINUTES_IN_HOUR))
 
 }
+
+    const val MINUTES_IN_HOUR = 60
