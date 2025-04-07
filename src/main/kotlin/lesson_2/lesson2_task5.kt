f4babf4b9bdcc2d94850c516dc341555a275b5c8
Sokolova <sokolovagb@gmail.com>
2025-04-07 19:55:53 +0300

    KS-2-5 design

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,15 +1,20 @@
 package org.example.lesson_2
 
 fun main() {
+
     val depositMoney = 70000
     val interestRate: Double = 16.7
     val depositYear = 20
+
     val monthSum = depositYear * monthInYear
     val rate: Double = interestRate / coef
     val rateYear = 1 + rate / 12
     val result = Math.pow(rateYear, monthSum.toDouble())
     val deposit = depositMoney * result
+
     println("%.3f".format(deposit.toDouble()))
+
 }
+
     const val monthInYear = 12
     const val coef = 100
