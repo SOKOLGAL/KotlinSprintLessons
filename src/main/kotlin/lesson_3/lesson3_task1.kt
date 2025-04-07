d2ccbd114c676e27a5138fb37eb92d6a88205f14
Sokolova <sokolovagb@gmail.com>
2025-04-07 20:03:17 +0300

    KS-3-1 greeting day evening

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val userName = "Галина"
+    var greeting = "Добрый день"
+
+    println("$greeting, $userName!")
+
+    greeting = "Добрый вечер"
+
+    println("$greeting, $userName!")
+
+}
