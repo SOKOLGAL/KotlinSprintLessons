05d495315b1caca7e85e397576127a66366662f9
Sokolova <sokolovagb@gmail.com>
2025-04-07 16:44:59 +0300

    KS-1-5 const

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -3,7 +3,6 @@ package org.example.lesson_1
 fun main() {
 
     val secondsInSpace: Int = 6480
-    val SECOND_IN_HOUR = 3600
     val hour = secondsInSpace / SECOND_IN_HOUR
     val secondsRemaining = secondsInSpace % SECOND_IN_HOUR
     val SECOND_IN_MINUTE = 60
@@ -11,3 +10,4 @@ fun main() {
     val second = secondsRemaining % SECOND_IN_MINUTE
     println("%02d:%02d:%02d".format(hour, minute, second))
 }
+    const val SECOND_IN_HOUR = 3600
