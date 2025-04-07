5a74870ccba653a979eddd4ec0f989fd0cb29712
Sokolova <sokolovagb@gmail.com>
2025-04-07 19:25:32 +0300

    KS-2-1 to float

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -5,8 +5,10 @@ fun main() {
     val student2 = 4
     val student3 = 3
     val student4 = 5
+    val numberOfStudents = 4
+
     val totalPoints = student1 + student2 + student3 + student4
-    val numberOfStudents = 4f
-    println(totalPoints / numberOfStudents)
+
+    println(totalPoints / numberOfStudents.toFloat())
 
 }
