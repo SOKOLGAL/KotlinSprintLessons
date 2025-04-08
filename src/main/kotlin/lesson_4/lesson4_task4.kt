49316fd9114abddf663bd4e7da9fe7b6d134424f
Sokolova <sokolovagb@gmail.com>
2025-04-08 20:41:27 +0300

    KS-4-4 training days

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -3,10 +3,10 @@ package org.example.lesson_4
 fun main() {
 
     val arrayOfExersises = arrayOf("для рук", "для ног", "для спины", "для пресса")
-    val trainingDay = 6
+    val trainingDay = 5
     val even = trainingDay % 2
-    val trainingHandsPress = even == 0
-    val trainingLegsBack = even !== 0
+    val trainingHandsPress = even !== 0
+    val trainingLegsBack = even == 1
 
     for (i in arrayOfExersises) {
             println("Упражнения $i: ${(trainingHandsPress) or (trainingLegsBack)}")
