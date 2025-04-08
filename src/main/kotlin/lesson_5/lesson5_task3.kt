c80961145a85de174a007cdae249b632447312f5
Sokolova <sokolovagb@gmail.com>
2025-04-08 21:40:33 +0300

    KS-5-3 winning number

---
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_5
+
+fun main() {
+
+    println("Примите участие в лотерее! Введите по очереди два числа от 0 до 42:")
+    val number1 = readln().toInt()
+    val number2 = readln().toInt()
+    val winNum1 = 20
+    val winNum2 = 35
+
+    if (number1 == winNum1 && number2 == winNum2) {
+        println("Поздравляем! Вы выиграли главный приз!")
+    } else if (number1 == winNum1 && number2 !== winNum2) {
+        println("Вы выиграли утешительный приз!")
+    }else if (number1 !== winNum1 && number2 == winNum2) {
+        println("Вы выиграли утешительный приз!")
+    } else println("Неудача!")
+}
