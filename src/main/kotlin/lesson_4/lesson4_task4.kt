cfec7cc3e9e2bbd275060722599dfb581db1fada
Sokolova <sokolovagb@gmail.com>
2025-04-08 20:36:26 +0300

    KS-4-4 training

---
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_4
+
+fun main() {
+
+    val arrayOfExersises = arrayOf("для рук", "для ног", "для спины", "для пресса")
+    val trainingDay = 6
+    val even = trainingDay % 2
+    val trainingHandsPress = even == 0
+    val trainingLegsBack = even !== 0
+
+    for (i in arrayOfExersises) {
+            println("Упражнения $i: ${(trainingHandsPress) or (trainingLegsBack)}")
+        }
+
+}
