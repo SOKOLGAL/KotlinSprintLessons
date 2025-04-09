e0df2a903d0838f4f32cb95c1a77f53b482905d6
Sokolova <sokolovagb@gmail.com>
2025-04-09 20:32:41 +0300

    KS-3-5 chess task

---
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -0,0 +1,23 @@
+package org.example.lesson_3
+
+fun main() {
+
+    val lineFromPlayer.toString = "D2 - D4"; 0
+
+    var from = "D2"
+    var where = "D4"
+    var moveNumber = 0
+
+    var from = "E2"
+    var where = "E4"
+    var moveNumber = 1
+
+    println("[$from-$where; $moveNumber]")
+
+    from = "D2"
+    where = "D3"
+    moveNumber++
+
+    println("[$from-$where; $moveNumber]")
+
+}
