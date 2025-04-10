f71f38b1b4323b7e8dfa7a8c5fe813a8e2381a73
Sokolova <sokolovagb@gmail.com>
2025-04-10 18:11:55 +0300

    KS-4-3 chang values to true

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -2,18 +2,20 @@ package org.example.lesson_4
 
 fun main() {
 
-    val isSunnyToday = "yes"
-    val isAwningOpenToday = "yes"
+    val weatherIsSunnyToday = true
+    val isAwningOpenToday = true
     val airHumidityToday: Int = 20
     val currentSeasonToday: String = "winter"
 
-    val resultAnalysisConditions = (isSunnyToday == IS_SUNNY) && (isAwningOpenToday == IS_AWNING_OPEN)
-            && (airHumidityToday == AIR_HUMIDITY) && (currentSeasonToday !== CURRENT_SEASON)
+    val resultAnalysisConditions = (weatherIsSunnyToday == WEATHER_IS_SUNNY) &&
+            (isAwningOpenToday == IS_AWNING_OPEN) &&
+            (airHumidityToday == AIR_HUMIDITY) &&
+            (currentSeasonToday !== CURRENT_SEASON)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $resultAnalysisConditions")
 
 }
-    const val IS_SUNNY = "yes"
-    const val IS_AWNING_OPEN = "yes"
+    const val WEATHER_IS_SUNNY = true
+    const val IS_AWNING_OPEN = true
     const val AIR_HUMIDITY = 20
     const val CURRENT_SEASON = "winter"
