d3a1c696cc73507e7660c8180b9602ac48bacbc0
Sokolova <sokolovagb@gmail.com>
2025-04-10 19:17:51 +0300

    KS-3-5 parse line chess

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,22 +2,13 @@ package org.example.lesson_3
 
 fun main() {
 
-    val lineFromPlayer.toString = "D2 - D4"; 0
+    val lineFromPlayer = "D2-D4;0".toString()
+    val from = "D2"
+    val where = "D4"
+    val moveNumber = 0
 
-    var from = "D2"
-    var where = "D4"
-    var moveNumber = 0
-
-    var from = "E2"
-    var where = "E4"
-    var moveNumber = 1
-
-    println("[$from-$where; $moveNumber]")
-
-    from = "D2"
-    where = "D3"
-    moveNumber++
-
-    println("[$from-$where; $moveNumber]")
+    println("$from")
+    println("$where")
+    println("$moveNumber")
 
 }
