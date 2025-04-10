1b02fd4698e0612c93aecbfd71e402834d08596d
Sokolova <sokolovagb@gmail.com>
2025-04-10 16:50:26 +0300

    KS-3-5 line win numbers

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -12,7 +12,9 @@ fun main() {
         println("Поздравляем! Вы выиграли главный приз!")
     } else if (number1 == winNum1 && number2 !== winNum2) {
         println("Вы выиграли утешительный приз!")
-    }else if (number1 !== winNum1 && number2 == winNum2) {
+    } else if (number1 !== winNum1 && number2 == winNum2) {
         println("Вы выиграли утешительный приз!")
     } else println("Неудача!")
+
+    println("Для победы нужны были числа: $winNum1 и $winNum2!")
 }
