d303f5504bbb3cc3b5b4bc1327318bd21521d01c
Sokolova <sokolovagb@gmail.com>
2025-04-11 19:46:47 +0300

    KS-5-5 intersect

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -6,33 +6,30 @@ fun main() {
     val number1 = readln().toInt()
     val number2 = readln().toInt()
     val number3 = readln().toInt()
-    val example1 = 3
-//        (1..42).random()
-    val example2 = 4
-//        (1..42).random()
-    val example3 = 5
-//        (1..42).random()
-    val winNum1 = example1
-    val winNum2 = example2
-    val winNum3 = example3
+    val example1 = (1..42).random()
+    val example2 = (1..42).random()
+    val example3 = (1..42).random()
     val num = listOf(number1, number2, number3)
     val winNum = listOf(example1, example2, example3)
     val result = num.intersect(winNum)
 
-    println(result)
-    if (num == winNum) {
+    if (number1 in result && number2 in result && number3 in result) {
         println("Поздравляем! Вы отгадали все числа и выиграли джекпот!")
     } else
-        if (number1 == winNum1 || number1 == winNum2 || number1 == winNum3
-        && number2 !== winNum1 || number2 !== winNum2 || number2 !== winNum3 &&
-        number1 !== winNum1 || number1 !== winNum2 && number2 == winNum1 || number2 == winNum2) {
-        println("Вы отгадали два числа и получаете крупный приз!")
-      }      else
-            if (number1 == winNum1 || number1 == winNum2 || number1 == winNum3
-                && number2 !== winNum1 || number2 !== winNum2 || number2 !== winNum3 &&
-                number1 !== winNum1 || number1 !== winNum2 && number2 == winNum1 || number2 == winNum2) {
+        if ((number1 in result && number2 in result && number3 !in result) ||
+            (number1 in result && number2 !in result && number3 in result) ||
+            (number1 !in result && number2 in result && number3 in result))
+        {
+            println("Вы отгадали два числа и получаете крупный приз!")
+        } else
+            if ((number1 == example1 || number1 == example2 || number1 == example3) ||
+                (number2 == example1 || number2 == example2 || number2 == example3) ||
+                (number3 == example1 || number3 == example2 || number3 == example3))
+            {
                 println("Вы отгадали одно число и получаете утешительный приз!")
-    }  else {println("Вы не угадали ни одного числа!")}
+            } else {
+                println("Вы не угадали ни одного числа!")
+            }
 
     println("Для победы нужны были числа: $example1, $example2 и $example3!")
 
