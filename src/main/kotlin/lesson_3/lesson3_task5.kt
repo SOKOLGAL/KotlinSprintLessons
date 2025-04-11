d403725efcb9f599af56e990eb8ce2a9db00156d
Sokolova <sokolovagb@gmail.com>
2025-04-11 21:28:58 +0300

    KS-3-5 split done

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -1,16 +1,17 @@
 package org.example.lesson_3
 
+import java.util.regex.Pattern
+
 fun main() {
 
     val lineFromPlayer = "D2-D4;0"
-    val fromLetter = lineFromPlayer.get(0)
-    val fromNum = lineFromPlayer.get(1)
-    val whereLetter = lineFromPlayer.get(3)
-    val whereNum = lineFromPlayer.get(4)
-    val moveNumber = lineFromPlayer.get(6)
+    val line = lineFromPlayer.split('-', ';')
+    val from = line.get(0)
+    val where = line.get(1)
+    val moveNumber = line.get(2)
 
-    println("$fromLetter$fromNum")
-    println("$whereLetter$whereNum")
+    println(from)
+    println(where)
     println(moveNumber)
 
 }
