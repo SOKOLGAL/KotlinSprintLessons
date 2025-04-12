0316c03d142ed0da209145c0a5c3a26a55628dfd
Sokolova <sokolovagb@gmail.com>
2025-04-12 16:13:42 +0300

    KS-3-5 simplified line

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -1,14 +1,12 @@
 package org.example.lesson_3
 
-import java.util.regex.Pattern
-
 fun main() {
 
     val lineFromPlayer = "D2-D4;0"
     val line = lineFromPlayer.split('-', ';')
-    val from = line.get(0)
-    val where = line.get(1)
-    val moveNumber = line.get(2)
+    val from = line[0]
+    val where = line[1]
+    val moveNumber = line[2]
 
     println(from)
     println(where)
