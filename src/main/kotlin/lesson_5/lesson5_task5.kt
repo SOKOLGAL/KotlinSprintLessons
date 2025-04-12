cae85167ed4167b2ca531f047ec8bbff7984faee
Sokolova <sokolovagb@gmail.com>
2025-04-12 18:24:35 +0300

    KS-5-5 not decided

--- src/main/kotlin/lesson_5/lesson5_task5.kt
+++ src/main/kotlin/lesson_5/lesson5_task5.kt
@@ -3,17 +3,17 @@ package org.example.lesson_5
 fun main() {
 
     println("Примите участие в лотерее! Введите по очереди три числа от 0 до 42:")
-    val number = repeat(NUM) {readln().toInt()}
-    val example = repeat(NUM) {(1..42).random()}
-    val num = listOf(number)
-    val winNum = listOf(example)
-    println("$number")
-    println("$winNum")
+
+    val num = listOf(repeat(NUM) { readln().toInt() })
+    val winNum = listOf(repeat(NUM) { (1..42).random() })
     val result = num.intersect(winNum)
 
-//    if (number1 in result && number2 in result && number3 in result) {
-//        println("Поздравляем! Вы отгадали все числа и выиграли джекпот!")
-//    } else
+    when (result) {
+        result -> "Поздравляем! Вы отгадали все числа и выиграли джекпот!"
+        (num !in result) -> "Вы не угадали ни одного числа!"
+        else -> "Вы не угадали ни одного числа!"
+}
+
 //        if ((number1 in result && number2 in result && number3 !in result) ||
 //            (number1 in result && number2 !in result && number3 in result) ||
 //            (number1 !in result && number2 in result && number3 in result))
@@ -25,11 +25,8 @@ fun main() {
 //                (number3 == example1 || number3 == example2 || number3 == example3))
 //            {
 //                println("Вы отгадали одно число и получаете утешительный приз!")
-//            } else {
-//                println("Вы не угадали ни одного числа!")
 //            }
 //
-    println("Для победы нужны были числа: $example1, $example2 и $example3!")
 
 }
 
