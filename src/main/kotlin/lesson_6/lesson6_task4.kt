b622ae1a5afc3cc4eb62ba50de4e5d0d7ae015e8
Sokolova <sokolovagb@gmail.com>
2025-04-19 20:55:10 +0300

    KS-6-4 do while

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -4,17 +4,19 @@ fun main() {
 
     val winNum = (1..9).random()
     println("Отгадайте число от 1 до 9:")
+    var numberOfAttempts = 5
 
-    for (i in 5 downTo 1) {
+
+    do {
         val num = readln().toInt()
-        if (num == winNum) {
-            println("Это была великолепная игра!")
-            break
-        } else if (num !== winNum) {
-            println("Неверно. Осталось еще ${i - 1} попыток")
-            continue
-        }
-    }
+        var result = num !== winNum
+        println("Неверно. Осталось еще ${numberOfAttempts-- - 1} попытки")
+        result = num == winNum
+
+    } while (numberOfAttempts > 0)
+
+    println("Это была великолепная игра!")
+
     println("Было загадано число: $winNum")
 
 }
