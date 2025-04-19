8171a45d2603eef4fdbe87f9fb5141581d92a4e2
Sokolova <sokolovagb@gmail.com>
2025-04-19 13:42:02 +0300

    KS-7-1 letters numbers

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -2,8 +2,8 @@ package org.example.lesson_7
 
 fun main() {
 
-    val passwordNum = List(NUMBER) { (1..9).random()}
-    val passwordLet = List(LETTER) { ('a'..'z').random()}
+    val passwordNum = List(NUMBER) { (1..9).random() }
+    val passwordLet = List(LETTER) { ('a'..'z').random() }
 
     println("Сгенерированный пароль: ${passwordLet[0]}${passwordNum[0]}${passwordLet[1]}${passwordNum[1]}${passwordLet[2]}${passwordNum[2]}")
 
