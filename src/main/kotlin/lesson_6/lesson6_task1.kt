bb2fb7d22e8e611f2157c9723b7a319f6b35502c
Sokolova <sokolovagb@gmail.com>
2025-04-19 20:02:48 +0300

    KS-6-1 do while

--- src/main/kotlin/lesson_6/lesson6_task1.kt
+++ src/main/kotlin/lesson_6/lesson6_task1.kt
@@ -7,12 +7,15 @@ fun main() {
     println("Создайте ваш пароль:")
     val password = readln()
 
-    while (login = loginNew) {
+    do {
         println("Для авторизации введите ваш логин:")
         val loginNew = readln()
+        login == loginNew
         println("Введите ваш пароль:")
         val passwordNew = readln()
-    }
+        password == passwordNew
+    } while (false)
 
     println("Авторизация прошла успешно")
+
 }
