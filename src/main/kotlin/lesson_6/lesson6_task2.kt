7e8c7b36393bd4c13f0c16ec8e434533a144e7db
Sokolova <sokolovagb@gmail.com>
2025-04-19 20:10:06 +0300

    KS-6-2 sleep

--- src/main/kotlin/lesson_6/lesson6_task2.kt
+++ src/main/kotlin/lesson_6/lesson6_task2.kt
@@ -3,12 +3,13 @@ package org.example.lesson_6
 fun main() {
 
     println("Введите количество секунд, которые нужно засечь:")
+
     val numberOfSeconds = readln().toLong()
     val seconds = numberOfSeconds * MILLISECONDS
-    do {
-        Thread.sleep(seconds)
-        println("Прошло $numberOfSeconds секунд")
-            } while (false)
+
+    Thread.sleep(seconds)
+
+    println("Прошло $numberOfSeconds секунд")
 
 }
 
