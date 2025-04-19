90bfbb6cc4172d1380fb36d7663accb5a019ecdd
Sokolova <sokolovagb@gmail.com>
2025-04-19 19:43:16 +0300

    KS-6-1 while

--- src/main/kotlin/lesson_6/lesson6_task1.kt
+++ src/main/kotlin/lesson_6/lesson6_task1.kt
@@ -3,29 +3,16 @@ package org.example.lesson_6
 fun main() {
 
     println("Создайте ваш логин:")
-    var login = readln()
+    val login = readln()
     println("Создайте ваш пароль:")
-    var password = readln()
-    val enterLoginPass = listOf(login, password)
+    val password = readln()
 
-    do {
-        do {
-            println("Для авторизации введите ваш логин:")
-            login = readln()
-            while (login !in enterLoginPass) {
-                println("Для авторизации введите ваш логин:")
-                login = readln()
-            }
-            println("Введите ваш пароль:")
-            password = readln()
-            while (password !in enterLoginPass) {
-                println("Введите ваш пароль:")
-                password = readln()
-            }
-        } while (false)
-
-    } while (false)
+    while (login = loginNew) {
+        println("Для авторизации введите ваш логин:")
+        val loginNew = readln()
+        println("Введите ваш пароль:")
+        val passwordNew = readln()
+    }
 
     println("Авторизация прошла успешно")
-
 }
