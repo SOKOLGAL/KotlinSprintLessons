9fefb50d97e10c715809458f7815c467cbb86726
Sokolova <sokolovagb@gmail.com>
2025-04-20 21:46:22 +0300

    KS-7-4 deleted range

--- src/main/kotlin/lesson_7/lesson7_task4.kt
+++ src/main/kotlin/lesson_7/lesson7_task4.kt
@@ -4,9 +4,8 @@ fun main() {
 
     println("Введите количество секунд, которые нужно засечь:")
     var numberOfSeconds = readln().toLong()
-    val range = numberOfSeconds downTo 1
 
-    for (i in range) {
+    for (i in numberOfSeconds downTo 0) {
         println("Осталось ${numberOfSeconds--} секунд")
         Thread.sleep(1000)
     }
