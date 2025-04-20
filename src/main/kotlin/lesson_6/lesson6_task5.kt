f6c518a300785eebc7c9473ad713ae0aefbb2711
Sokolova <sokolovagb@gmail.com>
2025-04-20 22:09:00 +0300

    KS-6-5 correct answer

--- src/main/kotlin/lesson_6/lesson6_task5.kt
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -12,13 +12,14 @@ fun main() {
         val summary = sum(example1, example2)
 
         println("Подтвердите, что вы не бот. Решите уравнение: $example1 + $example2 = ? Введите ваш ответ:")
-
         val solution = readln().toInt()
+        val result = (solution == summary) && (numberOfAttempts > 1)
+
         println("Неверно! Попробуйте еще ${(numberOfAttempts--) - 1} раза")
-        val result = (solution == summary) && (numberOfAttempts > 0)
         println("Добро пожаловать!")
 
     } while (result)
+
     println("Доступ запрещен.")
 
 }
