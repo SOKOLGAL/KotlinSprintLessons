ebe5ebc4da04d09e644991ac2d37776643baaf7a
Sokolova <sokolovagb@gmail.com>
2025-04-21 20:56:24 +0300

    KS-8-2 for if else

---
+++ src/main/kotlin/lesson_8/lesson8_task2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_8
+
+fun main() {
+
+    val arrayOfIngredients = arrayOf("яйцо", "помидор", "сыр", "грибы", "укроп", "петрушка", "соль", "перец", "болгарский перец", "сметана", "молоко", "лук")
+    println("Какой ингредиент вы хотите найти?")
+    val ingredient = readln()
+
+    for (i in arrayOfIngredients) {
+        if(ingredient in arrayOfIngredients) {
+            println("Ингредиент ${ingredient} в рецепте есть")
+            break
+     } else println("Такого ингредиента в рецепте нет")
+        break
+    }
+}
