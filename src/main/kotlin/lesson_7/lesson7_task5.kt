4d642768f811b1441a64a28fc9d512470bb349cb
Sokolova <sokolovagb@gmail.com>
2025-04-22 16:16:45 +0300

    KS-7-1 recording form

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -5,10 +5,20 @@ fun main() {
     println("Задайте размер генерируемого пароля (минимальная длина пароля $MINIMUM_PASSWORD_LENGTH символов):")
 
     val passwordLenght = readln().toInt()
+    val lowercaseLetters = 'a'..'z'
+    val capitalLetters = 'A'..'Z'
+    val numbers = '0'..'9'
+    val password =  mutableListOf(lowercaseLetters + capitalLetters + numbers)
+    
 
-    val password = List(passwordLenght) {
-        (('a'..'z') + ('A'..'Z') + ('0'..'9')).random()
-    }.joinToString("")
+//    password.add(lowercaseLetters.random())
+//    password.add(capitalLetters.random())
+//    password.add(numbers.random())
+println("$password")
+
+//    val password = List(passwordLenght) {
+//        (('a'..'z') + ('A'..'Z') + ('0'..'9')).random()
+//    }.joinToString("")
 
     println("Сгенерированный пароль: ${password}")
 
