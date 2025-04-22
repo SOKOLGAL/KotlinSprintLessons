a09564891054f1db8e7ce437653d710420123e65
Sokolova <sokolovagb@gmail.com>
2025-04-22 20:15:01 +0300

    KS-9-2 add ingredient

---
+++ src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson_9
+
+fun main() {
+
+    val mutableListOfIngredients = mutableListOf("яйцо", "помидор", "сыр")
+    println("В рецепте есть следующие ингредиенты: $mutableListOfIngredients")
+    println("Желаете добавить еще?")
+    val answer = readln()
+
+    if (answer == "да") {
+        println("Какой ингредиент вы хотите добавить?")
+        val addIngredient = readln()
+        mutableListOfIngredients.add(addIngredient)
+    }
+
+    println("Теперь в рецепте есть следующие ингредиенты: $mutableListOfIngredients")
+
+}
