7ce70fa2ef07235ee0df45fc7a08fa8abae3e063
Sokolova <sokolovagb@gmail.com>
2025-04-22 16:43:11 +0300

    KS-7-5 for gives the same symbols

--- src/main/kotlin/lesson_7/lesson7_task5.kt
+++ src/main/kotlin/lesson_7/lesson7_task5.kt
@@ -5,23 +5,24 @@ fun main() {
     println("Задайте размер генерируемого пароля (минимальная длина пароля $MINIMUM_PASSWORD_LENGTH символов):")
 
     val passwordLenght = readln().toInt()
-    val lowercaseLetters = 'a'..'z'
-    val capitalLetters = 'A'..'Z'
-    val numbers = '0'..'9'
-    val password =  mutableListOf(lowercaseLetters + capitalLetters + numbers)
-    
 
-//    password.add(lowercaseLetters.random())
-//    password.add(capitalLetters.random())
-//    password.add(numbers.random())
-println("$password")
+    val lowercaseLetters = ('a'..'z').random()
+    val capitalLetters = ('A'..'Z').random()
+    val numbers = ('0'..'9').random()
+    val password = mutableListOf(lowercaseLetters)
+    password.add(lowercaseLetters)
+    password.add(capitalLetters)
+    password.add(numbers)
 
-//    val password = List(passwordLenght) {
-//        (('a'..'z') + ('A'..'Z') + ('0'..'9')).random()
-//    }.joinToString("")
+    val pass = (('a'..'z') + ('A'..'Z') + ('0'..'9')).random()
+    for (i in passwordLenght downTo FREE_PASSWORD_LENGTH) {
+        password.add(pass)
+    }
+    password.shuffle()
 
     println("Сгенерированный пароль: ${password}")
 
 }
 
 const val MINIMUM_PASSWORD_LENGTH = 6
+const val FREE_PASSWORD_LENGTH = 4
