d45d747d00adbfc987554b4ab7f8f22fd6f50235
Sokolova <sokolovagb@gmail.com>
2025-04-22 14:45:35 +0300

    KS-8-3 ctrl alt l

--- src/main/kotlin/lesson_8/lesson8_task3.kt
+++ src/main/kotlin/lesson_8/lesson8_task3.kt
@@ -2,13 +2,26 @@ package org.example.lesson_8
 
 fun main() {
 
-    val arrayOfIngredients = arrayOf("яйцо", "помидор", "сыр", "грибы", "укроп", "петрушка", "соль", "перец", "болгарский перец", "сметана", "молоко", "лук")
+    val arrayOfIngredients = arrayOf(
+        "яйцо",
+        "помидор",
+        "сыр",
+        "грибы",
+        "укроп",
+        "петрушка",
+        "соль",
+        "перец",
+        "болгарский перец",
+        "сметана",
+        "молоко",
+        "лук"
+    )
     println("Какой ингредиент вы хотите найти?")
     val ingredient = readln()
     val hasIngredient = arrayOfIngredients.contains(ingredient)
 
-    if(hasIngredient == true) {
+    if (hasIngredient == true) {
         println("Ингредиент ${ingredient} в рецепте есть")
-        } else println("Такого ингредиента в рецепте нет")
+    } else println("Такого ингредиента в рецепте нет")
 
 }
