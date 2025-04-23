66ba064de1ebe6c797fef3e879f754fb9f2aa6d1
Sokolova <sokolovagb@gmail.com>
2025-04-23 18:52:54 +0300

    KS-9-4 split

--- src/main/kotlin/lesson_9/lesson9_task4.kt
+++ src/main/kotlin/lesson_9/lesson9_task4.kt
@@ -4,8 +4,11 @@ fun main() {
 
     println("Введите 5 названий ингредиентов блюда в одной строке, разделяя их запятыми (с пробелом):")
     val fiveNamesOfIngredients = readln()
-    val listIngredients = fiveNamesOfIngredients.toMutableList()
+    val listIngredients = fiveNamesOfIngredients.split(',')
+    listIngredients.toMutableList()
+
     val newList: Any = listIngredients.sorted()
+
 
 // мука, яйца, сахар, яблоки, ваниль
 
