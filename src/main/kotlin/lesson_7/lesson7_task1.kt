973ac80b5d34525a34df3b64a8572ccc9d576629
Sokolova <sokolovagb@gmail.com>
2025-04-23 17:28:48 +0300

    KS-7-1 plus equals

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -4,13 +4,17 @@ fun main() {
 
     val numberOfCharacters = 6 / TWO_CHARACTER_IN_ONE_PASS_FOR
     val passwordLet = 'a'..'z'
-    val passwordNum = '1'..'9'
-    val password =
+    val passwordNum = 1..9
+    var password = passwordLet + passwordNum
 
     for (i in 1..numberOfCharacters) {
         password += passwordLet.random()
         password += passwordNum.random()
     }
+//    for (i in 0..numberOfCharacters) {
+//        println("Сгенерированный пароль: ${passwordLet.random()}")
+//        println("Сгенерированный пароль: ${passwordNum.random()}")
+//    }
 
     println("Сгенерированный пароль: ${password}")
 
