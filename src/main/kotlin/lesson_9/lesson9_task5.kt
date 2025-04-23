87a7c8a7f097226b84472c111de6e3b0934f7cd9
Sokolova <sokolovagb@gmail.com>
2025-04-23 20:16:32 +0300

    KS-9-5 filter

---
+++ src/main/kotlin/lesson_9/lesson9_task5.kt
@@ -0,0 +1,29 @@
+package org.example.lesson_9
+
+fun main() {
+
+    println("Введите по одному 5 ингредиентов:")
+    val mutableListOfIngredients = mutableListOf("")
+
+    for (i in QUANTITY_OF_INGREDIENTS downTo 1) {
+        val addIngredient = readln()
+        mutableListOfIngredients.add(addIngredient)
+        mutableListOfIngredients.filter {
+                it == addIngredient
+            }
+            println("Такой ингредиент уже введен")
+        }
+
+//        equals(addIngredient)
+
+    }
+    mutableListOfIngredients.sort()
+
+    println("$mutableListOfIngredients")
+
+
+
+
+}
+
+const val QUANTITY_OF_INGREDIENTS = 5
