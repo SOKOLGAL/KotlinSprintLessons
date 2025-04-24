7224a71dbf774d2a7421e7b88ee62a29c3e0b1bd
Sokolova <sokolovagb@gmail.com>
2025-04-24 12:50:20 +0300

    KS-7-1 var password double quotes

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -7,7 +7,7 @@ fun main() {
     val passwordNum = 1..9
     var password = ""
 
-    for (i in 1..(numberOfCharacters/ TWO_CHARACTER_IN_ONE_PASS_FOR)) {
+    for (i in 1..(numberOfCharacters / TWO_CHARACTER_IN_ONE_PASS_FOR)) {
         password += passwordLet.random()
         password += passwordNum.random()
     }
