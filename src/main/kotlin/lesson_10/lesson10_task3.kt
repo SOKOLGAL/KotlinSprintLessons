65e5b583e5d798c63362b9edb0cbb0669888a042
Sokolova <sokolovagb@gmail.com>
2025-04-26 17:32:38 +0300

    KS-10-3 add character to end

--- src/main/kotlin/lesson_10/lesson10_task3.kt
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -5,20 +5,24 @@ fun main() {
     println("Задайте длину пароля:")
 
     val passwordLength = readln().toInt()
-    val passwordGen = passwordGeneration(passwordLength.toString())
+    val passwordGen = generatePassword(passwordLength)
+
     println(passwordGen)
 
 }
 
-fun passwordGeneration(passwordLength: String): String? {
-    val numberOfCharacters = passwordLength.toInt()
-    val specialCharacters = mutableListOf('!', '"', '#', '$', '%', '&', '`', '(', ')', '*', '+', '-', '/', ' ')
+fun generatePassword(passwordLength: Int): String {
+    val specialCharacters = ' '..'/'
     val passwordNum = 0..9
     var password: String = ""
 
-    for (i in 1..numberOfCharacters / TWO_CHARACTER_IN_ONE_PASS_FOR) {
+    for (i in 1..passwordLength / TWO_CHARACTER_IN_ONE_PASS_FOR) {
         password += passwordNum.random()
         password += specialCharacters.random()
+//        if (passwordLength % 2 == 0) {
+//
+//        } else password += specialCharacters.random()
+
     }
 
     return password
