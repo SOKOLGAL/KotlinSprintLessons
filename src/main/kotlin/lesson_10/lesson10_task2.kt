15d695264fac591edb3cd4e1b2c8c444c80b57cd
Sokolova <sokolovagb@gmail.com>
2025-04-26 17:04:10 +0300

    KS-10-2 const short form of function

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -1,23 +1,24 @@
 package org.example.lesson_10
 
 fun main() {
+
     println("Придумайте логин:")
     val login = readln()
+
     println("Придумайте пароль:")
     val password = readln()
-    val loginNumberOfCharacters = login.toCharArray().count()
-    val passwordNumberOfCharacters = password.toCharArray().count()
-    val result = validationLength(loginNumberOfCharacters, passwordNumberOfCharacters)
 
-    if (result == false) {
-        println("Логин или пароль недостаточно длинные")
-    } else println("Приветствуем вас")
+    val loginNumberOfCharacters = login.length
+    val passwordNumberOfCharacters = password.length
+    val result = isLengthValid(loginNumberOfCharacters, passwordNumberOfCharacters)
+
+    if (result) {
+        println("Приветствуем вас")
+    } else println("Логин или пароль недостаточно длинные")
 
 }
 
-fun validationLength(loginNumberOfCharacters: Int?, passwordNumberOfCharacters: Int?): Boolean {
-    val minimumNumberOfCharacters = 4
-    val result =
-        loginNumberOfCharacters!! >= minimumNumberOfCharacters || passwordNumberOfCharacters!! >= minimumNumberOfCharacters
-    return result
-}
+fun isLengthValid(loginNumberOfCharacters: Int?, passwordNumberOfCharacters: Int?): Boolean =
+    loginNumberOfCharacters!! >= MINIMUM_NUMBER_OF_CHARACTERS && passwordNumberOfCharacters!! >= MINIMUM_NUMBER_OF_CHARACTERS
+
+const val MINIMUM_NUMBER_OF_CHARACTERS = 4
