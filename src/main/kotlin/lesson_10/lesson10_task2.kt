e6d2a664f7f8bb5f909517df6a474e28c4ff58ec
Sokolova <sokolovagb@gmail.com>
2025-04-27 16:18:51 +0300

    KS-10-2 not work

--- src/main/kotlin/lesson_10/lesson10_task2.kt
+++ src/main/kotlin/lesson_10/lesson10_task2.kt
@@ -1,24 +1,24 @@
 package org.example.lesson_10
 
 fun main() {
-
-    println("Придумайте логин:")
-    val login = readln()
-
-    println("Придумайте пароль:")
-    val password = readln()
-
-    val loginNumberOfCharacters = login.length
-    val passwordNumberOfCharacters = password.length
-    val result = isLengthValid(loginNumberOfCharacters, passwordNumberOfCharacters)
-
-    if (result) {
-        println("Приветствуем вас")
-    } else println("Логин или пароль недостаточно длинные")
+    isLengthValid()
 
 }
 
-fun isLengthValid(loginNumberOfCharacters: Int?, passwordNumberOfCharacters: Int?): Boolean =
-    loginNumberOfCharacters!! >= MINIMUM_NUMBER_OF_CHARACTERS && passwordNumberOfCharacters!! >= MINIMUM_NUMBER_OF_CHARACTERS
+fun isLengthValid(): Boolean {
+    println("Придумайте логин:")
+    val login = readln()
+    println("Придумайте пароль:")
+    val password = readln()
+    login.length
+    password.length
+    val result =
+        login.toInt() >= MINIMUM_NUMBER_OF_CHARACTERS || password.toInt() >= MINIMUM_NUMBER_OF_CHARACTERS
+    if (true) {
+        println("Приветствуем вас")
+    } else println("Логин или пароль недостаточно длинные")
+
+    return result
+}
 
 const val MINIMUM_NUMBER_OF_CHARACTERS = 4
