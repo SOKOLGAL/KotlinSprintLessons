95e0409d7147205b849090011a135d45f1009fe0
Sokolova <sokolovagb@gmail.com>
2025-04-28 19:57:04 +0300

    KS-11-1 id login password mail

---
+++ src/main/kotlin/lesson_11/main.kt
@@ -0,0 +1,29 @@
+package org.example.lesson_11
+
+fun main() {
+
+    val user1: User = User(
+        id = 1,
+        login = "LittlePrince",
+        password = "Ro_Sa_Exup",
+        mail = "littleprince@mail.ru",
+    )
+
+    val user2: User = User(
+        id = 2,
+        login = "Helen_1980",
+        password = "NeLeH_98",
+        mail = "Helen@yandex.ru",
+    )
+
+    println(user1.id)
+    println(user1.login)
+    println(user1.password)
+    println(user1.mail)
+    println()
+    println(user2.id)
+    println(user2.login)
+    println(user2.password)
+    println(user2.mail)
+
+}
