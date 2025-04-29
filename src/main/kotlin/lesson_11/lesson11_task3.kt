a465d3a80d8775d9f4097ecb99773315cd47d0f2
Sokolova <sokolovagb@gmail.com>
2025-04-29 13:42:51 +0300

    KS-11-3 logic correct?

--- src/main/kotlin/lesson_11/lesson11_task3.kt
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -2,29 +2,55 @@ package org.example.lesson_11
 
 fun main() {
 
-    val member: Room = Room(
-        cover = "обложка",
-        name = "Piter",
-        listOfParticipants = listOf("Kate", "Volga", "Nico"),
-        textBadge = listOf("")
+    val user1: Room = Room(
+        nickname = "",
     )
+
+    user1.addUser()
+
+    user1.statusUpdate()
 
 }
 
 class Room(
-    val cover: String,
-    val name: String,
-    var listOfParticipants: List<String>,
-    val textBadge: List<String> = listOf("разговаривает", "микрофон выключен", "пользователь заглушен"),
+    val cover: String = "Картинка",
+    val roomName: String = "Комната просто поболтать",
+    var nickname: String,
+    var avatar: String = "Ава",
+    var listOfParticipants: List<String> = listOf("Kate", "Volga", "Nico"),
+    var textBadge: List<String> = listOf("разговаривает", "микрофон выключен", "пользователь заглушен"),
 ) {
 
-    fun addMember(name: String): List<String> {
-        listOfParticipants = listOf(readln())
-    return participants
+    fun addUser() {
+        println("Для участия в $roomName заполните след данные:")
+
+        println("Введите ваше имя:")
+        nickname = readln()
+
+        println("Список участников комнаты: $listOfParticipants")
+
+        println("Добавьте аватар:")
+        avatar = readln()
+
+        println("Выберете статус:")
+        textBadge = listOf(readln())
+
+        println("Получены следующие данные:")
+        println(cover)
+        println(roomName)
+        println(nickname)
+        println(avatar)
+        println(listOfParticipants)
+        println(textBadge)
+
     }
 
     fun statusUpdate() {
-
+        println("Для изменения статуса введите имя:")
+        nickname = readln()
+        println("Изменить статус:")
+        textBadge = listOf(readln())
+        println("Статус изменён")
     }
 
 }
