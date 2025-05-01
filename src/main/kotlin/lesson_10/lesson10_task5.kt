aee0770d892f0c60dcc2785ec78ae9646b263eac
Sokolova <sokolovagb@gmail.com>
2025-05-01 21:08:15 +0300

    KS-10-5 null how to use

--- src/main/kotlin/lesson_10/lesson10_task5.kt
+++ src/main/kotlin/lesson_10/lesson10_task5.kt
@@ -6,36 +6,41 @@ fun main() {
     val login = readln()
     println("Введите пароль:")
     val password = readln()
-    passAuthorization()
+    passAuthorization(login, password)
     generateToken()
-    getBasket()
+    getBasket(println().toString())
+
 
 }
 
-fun passAuthorization(login: String, password: String): String {
-
-    val successfulAuthorization = login == USER_LOGIN && password == USER_PASSWORD
-
-    if (successfulAuthorization) {
-        generateToken()
-    } else println("Неудачная авторизация")
-    return generateToken()
-//    token
+fun passAuthorization(login: String, password: String): String? {
+    var token: String? = ""
+    if (login == USER_LOGIN && password == USER_PASSWORD) {
+        token = generateToken()
+    } else token = null
+    return token
 }
 
 fun generateToken(): String {
     val tokenLength = 32
     val letter = 'a'..'z'
     val number = '0'..'9'
-    var token: String = ""
+    val token: MutableList<Char> = mutableListOf()
     for (i in 1..tokenLength / TWO_CHARACTER_IN_ONE_PASS_FOR) {
         token += (letter.random())
         token += (number.random())
     }
-    return token
+    if (tokenLength % 2 != 0) {
+        token += (number.random())
+    }
+    token.shuffle()
+       return token.toString()
 }
 
 fun getBasket(token: String): List<String> {
+    if (token == null) {
+        println("Неудачная авторизация")
+    } else println(listOf("Ботинки", "Носки", "Рубашка"))
     return listOf("Ботинки", "Носки", "Рубашка")
 }
 
