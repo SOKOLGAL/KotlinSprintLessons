491db650acfed4513fe5dee7a3d91a42801a8d67
Sokolova <sokolovagb@gmail.com>
2025-05-02 15:52:11 +0300

    KS-12-3 from kelvins to celsius

---
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -0,0 +1,28 @@
+package org.example.lesson_12
+
+fun main() {
+
+    val weather1 = SaveWeather()
+    weather1.daytimeTemperature = 300
+    weather1.nightTemperature = 275
+    weather1.precipitationDuringDay = false
+
+    weather1.weatherTodayTomorrow()
+
+}
+
+class SaveWeather(
+) {
+    var daytimeTemperature: Int = 15
+    var nightTemperature: Int = 2
+    var precipitationDuringDay: Boolean = false
+
+    fun weatherTodayTomorrow() {
+        println(
+            "Дневная температура ${daytimeTemperature - CONVERSION_TO_CELSIUS}, " +
+                    "ночная температура ${nightTemperature - CONVERSION_TO_CELSIUS}, " +
+                    "осадки $precipitationDuringDay")
+    }
+}
+
+const val CONVERSION_TO_CELSIUS = 273
