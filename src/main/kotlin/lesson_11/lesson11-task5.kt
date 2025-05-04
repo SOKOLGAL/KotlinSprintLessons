fc19f5dec9633e9b144c02a933e624e21d70b901
Sokolova <sokolovagb@gmail.com>
2025-05-04 20:18:30 +0300

    KS-11-5 class methods

--- src/main/kotlin/lesson_11/lesson11-task5.kt
+++ src/main/kotlin/lesson_11/lesson11-task5.kt
@@ -1,31 +1,27 @@
 package org.example.lesson_11
 
 fun main() {
-
+      ForumWork()
 }
 
-class ForumWork(
-    val forumList: MutableList<ForumMember> = mutableListOf(),
-    val forumMessage: MutableList<ForumMessage> = mutableListOf(),
-) {
-        fun createNewUser(userName: String): ForumMember {
-            println("Для регистрации на ФОРУМе введите ваше имя:")
-            forumList.add(ForumMember(userId = (1..1000).random(), userName = readln()))
-            return ForumMember(userId = (1..1000).random(), userName = println().toString())
-        }
+class ForumWork() {
+    val forumList: MutableList<ForumMember> = mutableListOf()
+    val forumMessage: MutableList<ForumMessage> = mutableListOf()
 
-        fun createNewMessage(userId: Int) {
-            val charPool: List<Char> = ('a'..'z') + ('A'..'Z')
-//            val new1 = (1..STRING_LENGTH)
-//                .map { Random.nextInt(0, charPool.size).let {charPool[it]}.joinToString("") }
-            val new = List(STRING_LENGTH) { charPool.random()}.joinToString("")
-
-        }
-
-        fun printThread(userName: String, message: String) {
-            return println("$userName: $message")
-        }
+    fun createNewUser(userName: String): ForumMember {
+        println("Для регистрации на ФОРУМе введите ваше имя:")
+        forumList.add(ForumMember(userId = (1..1000).random(), userName = readln()))
+        return ForumMember(userId = (1..1000).random(), userName = println().toString())
     }
+
+    fun createNewMessage(userId: Int): ForumMessage {
+        return
+    }
+
+    fun printThread(userName: String, message: String) {
+        return println("$userName: $message")
+    }
+}
 
 const val STRING_LENGTH = 10
 
@@ -36,7 +32,7 @@ class ForumMember(
 }
 
 class ForumMessage(
-    val authorId: Boolean,
+    val authorId: Int,
     val message: String,
 ) {
 }
