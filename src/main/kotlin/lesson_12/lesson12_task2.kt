89f03062d43db3e0ec101562b102690f58a87a3b
Sokolova <sokolovagb@gmail.com>
2025-05-04 13:52:55 +0300

    KS-12-2 var

--- src/main/kotlin/lesson_12/lesson12_task2.kt
+++ src/main/kotlin/lesson_12/lesson12_task2.kt
@@ -3,20 +3,15 @@ package org.example.lesson_12
 fun main() {
 
     val weather1 = SaveWeatherByDays(17, 8, true)
-
     weather1.weatherTodayTomorrow()
 
 }
 
 class SaveWeatherByDays(
-    daytimeTemperature: Int,
-    nightTemperature: Int,
-    precipitationDuringDay: Boolean,
+    var daytimeTemperature: Int,
+    var nightTemperature: Int,
+    var precipitationDuringDay: Boolean,
 ) {
-    var daytimeTemperature = daytimeTemperature
-    var nightTemperature = nightTemperature
-    var precipitationDuringDay = precipitationDuringDay
-
     fun weatherTodayTomorrow() {
         println("Дневная температура $daytimeTemperature, ночная температура $nightTemperature, осадки $precipitationDuringDay")
     }
