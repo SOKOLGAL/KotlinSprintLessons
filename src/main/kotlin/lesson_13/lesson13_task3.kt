e0a4f8f9b3022159e5098dc2657c222776282e14
Sokolova <sokolovagb@gmail.com>
2025-05-05 20:29:23 +0300

    KS-13-3 no null no simultaneously add

--- src/main/kotlin/lesson_13/lesson13_task3.kt
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -1,51 +1,35 @@
 package org.example.lesson_13
 
 fun main() {
-    
-    val contactList: MutableList<NewContactCreate> = mutableListOf(NewContactCreate(
-        name = TODO(),
-        phoneNumber = TODO(),
-        company = TODO()
-    ))
-    var newContact = NewContactCreate(
+
+    val newContact1 = NewContactCreate(
         name = "Мария",
         phoneNumber = "88009009050".toLong(),
         company = "Вчерашние новости"
     )
-    newContact = NewContactCreate(
+    val newContact2 = NewContactCreate(
         name = "Константин",
         phoneNumber = "88005005050".toLong(),
         company = "null"
     )
-    newContact = NewContactCreate(
+    val newContact3 = NewContactCreate(
         name = "Петр",
         phoneNumber = "88004681357".toLong(),
         company = ""
     )
-    newContact = NewContactCreate(
+    val newContact4 = NewContactCreate(
         name = "Дмитрий",
         phoneNumber = "88001000100".toLong(),
         company = "Зеленый луг"
     )
-    newContact = NewContactCreate(
+    val newContact5 = NewContactCreate(
         name = "Екатерина",
         phoneNumber = "88007770077".toLong(),
         company = ""
     )
-       for (i in 0 .. 5) {
-           contactList.add(MutableList(5) {newContact})
-       }
-        
-        
-        
-        
-//     val contactList = listOf(newContact1, newContact2, newContact3, newContact4, newContact5)
-
-//    val contactList1: MutableList<NewContactCreate> = mutableListOf()
-//    val contactList1: MutableList<NewContactCreate> = mutableListOf(newContact1,newContact2,newContact3,newContact4,newContact5)
-
-
-    val listOfAllCompanies = contact.map {
+    val contactList = listOf(newContact1, newContact2, newContact3, newContact4, newContact5)
+//    company ?: "<не указано>
+    val  listOfAllCompanies = contactList.map {
         it.company
     }
     println(listOfAllCompanies.toString())
@@ -56,7 +40,4 @@ class NewContactCreate(
     val phoneNumber: Long?,
     val company: String?,
 ) {
-//    val contactList: MutableList<NewContactCreate> = mutableListOf()
-
-    val replacement: String = company ?: "<не указано>"
 }
