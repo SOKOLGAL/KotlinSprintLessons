58e24611746b2e18621d8b32dad30c4dccbabe65
Sokolova <sokolovagb@gmail.com>
2025-05-05 21:57:50 +0300

    KS-13-4 one contact add number

---
+++ src/main/kotlin/lesson_13/lesson13_task4.kt
@@ -0,0 +1,28 @@
+package org.example.lesson_13
+
+fun main() {
+
+    val newContact1 = TelephoneDirectory(
+        name = readln(),
+        phoneNumber = readln().toLongOrNull(),
+        company = readln()
+    )
+    val contactList =listOf(newContact1)
+    if (newContact1.phoneNumber == null) {
+        println("Номер телефона не введён")
+        contactList  // запись не добавляется
+    }
+    newContact1.contactCard()
+}
+
+class TelephoneDirectory(
+    val name: String?,
+    val phoneNumber: Long?,
+    val company: String?,
+) {
+    val contactList: MutableList<String> = mutableListOf()
+
+    fun contactCard() {
+        println("- Имя: $name\n- Номер: ${phoneNumber ?: "Номер телефона не введён"}\n- Компания: ${company ?: "null"}")
+    }
+}
