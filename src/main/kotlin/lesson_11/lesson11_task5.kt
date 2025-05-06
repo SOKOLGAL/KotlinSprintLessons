b8878a56bd74d7fc795ff8993f6d75ebd1ee1612
Sokolova <sokolovagb@gmail.com>
2025-05-06 14:24:46 +0300

    KS-11-5 do not work

--- src/main/kotlin/lesson_11/lesson11_task5.kt
+++ src/main/kotlin/lesson_11/lesson11_task5.kt
@@ -1,24 +1,44 @@
 package org.example.lesson_11
 
 fun main() {
+
+    val user1: ForumWork = ForumWork()
+    val user2: ForumWork = ForumWork()
+    user1.createNewUser(readln())
+   val user1Message = readln()
+    user2.createNewUser(readln())
+    val user2Message = readln()
+
 }
 
 class ForumWork() {
     val forumList: MutableList<ForumMember> = mutableListOf()
     val forumMessage: MutableList<ForumMessage> = mutableListOf()
 
-    fun createNewUser(userName: String): ForumMember {
+    fun createNewUser(userName: String) {
         println("Для регистрации на ФОРУМе введите ваше имя:")
         forumList.add(ForumMember(userId = (1..1000).random(), userName = readln()))
-        return ForumMember(userId = (1..1000).random(), userName = println().toString())
+          }
+
+    fun createNewMessage(userId: Int) {
+        val findUser= forumList.map {
+        it.userId
+    }
+        if (userId in findUser) {
+            println("Введите ваше сообщение:")
+            val message: ForumMessage = readln()
+            forumMessage.add(message)
+        } else println("Пользователь не найден")
     }
 
-    fun createNewMessage(userId: Int): ForumMessage {
-        return
-    }
-
-    fun printThread(userName: String, message: String) {
-        return println("$userName: $message")
+    fun printThread(userName: String, message: ForumMessage) {
+        val findUser= forumList.map {
+            it.userName
+        }
+        val findMessage= forumMessage.map {
+            it.message
+        }
+        println("$findUser: $findMessage")
     }
 }
 
