d20a68cc6372f37081bb639f3ca33a99f3291666
Sokolova <sokolovagb@gmail.com>
2025-05-06 22:18:55 +0300

    KS-12-3 what is it?

--- src/main/kotlin/lesson_12/lesson12_task3.kt
+++ src/main/kotlin/lesson_12/lesson12_task3.kt
@@ -1,28 +1,28 @@
 package org.example.lesson_12
-
-fun main() {
-
-    val weather1 = SaveWeather(300, 275, true)
-    weather1.weatherTodayTomorrow()
-
-}
-
-class SaveWeather(
-    kelvinDayTemperature: Int,
-    kelvinNightTemperature: Int,
-    precipitationDuringDay: Boolean,
-) {
-    val daytimeTemperature: Int = kelvinDayTemperature - CONVERSION_TO_CELSIUS
-    val nightTemperature: Int = kelvinNightTemperature - CONVERSION_TO_CELSIUS
-    val precipitationDuringDay: Boolean = precipitationDuringDay
-
-    fun weatherTodayTomorrow() {
-        println(
-            "Дневная температура $daytimeTemperature, " +
-                    "ночная температура $nightTemperature, " +
-                    "осадки $precipitationDuringDay"
-        )
-    }
-}
-
-const val CONVERSION_TO_CELSIUS = 273
+//
+//fun main() {
+//
+//    val weather1 = SaveWeather(300, 275, true)
+//    weather1.weatherTodayTomorrow()
+//
+//}
+//
+//class SaveWeather(
+//    kelvinDayTemperature: Int,
+//    kelvinNightTemperature: Int,
+//    precipitationDuringDay: Boolean,
+//) {
+//    val daytimeTemperature: Int = kelvinDayTemperature - CONVERSION_TO_CELSIUS
+//    val nightTemperature: Int = kelvinNightTemperature - CONVERSION_TO_CELSIUS
+//    val precipitationDuringDay: Boolean = precipitationDuringDay
+//
+//    fun weatherTodayTomorrow() {
+//        println(
+//            "Дневная температура $daytimeTemperature, " +
+//                    "ночная температура $nightTemperature, " +
+//                    "осадки $precipitationDuringDay"
+//        )
+//    }
+//}
+//
+//const val CONVERSION_TO_CELSIUS = 273
