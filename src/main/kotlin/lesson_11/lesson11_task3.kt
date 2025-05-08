4389566a199eb8db1425423b422669800da651cc
Sokolova <sokolovagb@gmail.com>
2025-05-08 18:37:10 +0300

    KS-11-3 id

--- src/main/kotlin/lesson_11/lesson11_task3.kt
+++ src/main/kotlin/lesson_11/lesson11_task3.kt
@@ -4,18 +4,24 @@ fun main() {
 
     println("Введите ваше имя:")
     val user1: Participant = Participant(
-        id = 1,
-        nickname = readln(),
+        userId = TODO(),
+        nickName = readln(),
+    )
+    val add = Room()
+    add.addUser(
+        participant = user1
     )
     println("Выберете статус:")
     user1.textBadge = readln()
 
     println("Для изменения статуса введите имя:")
-    user1.nickname = readln()
+    user1.nickName = readln()
 
     println("Введите новый статус:")
-    user1.statusUpdate(readln())
-
+    add.statusUpdate(
+        nickName = user1,
+        newStatus = readln(),
+    )
 }
 
 class Room(
@@ -23,26 +29,31 @@ class Room(
     val roomName: String = "Комната просто поболтать",
     val listOfParticipants: MutableList<Participant> = mutableListOf(),
 ) {
-    fun addUser(Participant: String) {
-        listOfParticipants.plus(Participant)
+
+    fun addUser(participant: Participant) {
+        val findId = listOfParticipants.contains(participant)
+        if (findId) {
+            listOfParticipants.add(participant)
+            println("Пользователь добавлен")
+        }
     }
 
-    fun statusUpdate(id: Int, newStatus: String) {
-
-        val findId = listOfParticipants.find { id -> id.equals(id) }
-        if (findId != null) {
-            findId.textBadge = newStatus
-        }
-        println("Статус изменён на $newStatus")
+    fun statusUpdate(nickName: Participant, newStatus: String) {
+        val findNickName = listOfParticipants.contains(nickName)
+        if (findNickName) {
+            nickName.textBadge = newStatus
+            println("Статус изменён: $newStatus")
+        } else println("Статус не изменён")
     }
 }
 
 class Participant(
-    var id: Int,
+    var userId: Int,
     val avatar: String = "Ава",
-    var nickname: String,
+    var nickName: String,
     var textBadge: String = "микрофон выключен",
 ) {
+    val id = userId++
     fun statusUpdate(newStatus: String) {
         textBadge = newStatus
         println("Статус изменён на $newStatus")
