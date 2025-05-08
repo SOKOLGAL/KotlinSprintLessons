40febeb1c6c7e9fbe7acce098005efba4a00c5a3
Sokolova <sokolovagb@gmail.com>
2025-05-08 16:12:17 +0300

    KS-13-5 try catch

--- src/main/kotlin/lesson_13/lesson13_task5.kt
+++ src/main/kotlin/lesson_13/lesson13_task5.kt
@@ -3,9 +3,10 @@ package org.example.lesson_13
 fun main() {
 
     val newContact1 = TelephoneBook(phoneNumber = readln())
-    val number: Long? = newContact1.phoneNumber.toLongOrNull()
-    if (number == null) {
-        println("NumberFormatException")
+    val num = try {
+        val number = newContact1.phoneNumber.toLong()
+    } catch (e: Exception) {
+        println(e.message)
     }
 
 }
