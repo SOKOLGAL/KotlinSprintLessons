c33334208b2a9cae93ab52e98879d00acff477a0
Sokolova <sokolovagb@gmail.com>
2025-05-09 18:00:42 +0300

    KS-14-3 what need change

--- src/main/kotlin/lesson_14/lesson14_task3.kt
+++ src/main/kotlin/lesson_14/lesson14_task3.kt
@@ -4,23 +4,23 @@ import kotlin.math.pow
 
 fun main() {
 
-    val circle1 = Circle(COLOR_W, 15)
-    val circle2 = Circle(COLOR_B, 10)
-    val circle3 = Circle(COLOR_B, 5)
-    val rectangle1 = Rectangle(COLOR_B, 3, 7)
-    val rectangle2 = Rectangle(COLOR_W, 5, 9)
-    val rectangle3 = Rectangle(COLOR_B, 4, 8)
+    val circle1 = Circle(COLOR_W, 15.0)
+    val circle2 = Circle(COLOR_B, 10.0)
+    val circle3 = Circle(COLOR_B, 5.0)
+    val rectangle1 = Rectangle(COLOR_B, 3.0, 7.0)
+    val rectangle2 = Rectangle(COLOR_W, 5.0, 9.0)
+    val rectangle3 = Rectangle(COLOR_B, 4.0, 8.0)
 
     val generalList = mutableListOf(circle1, circle2, circle3, rectangle1, rectangle2, rectangle3)
-    val color = Figure().color
+    val color = ""
     val perimeter = generalList.filter {
         color == COLOR_B
     }
     val area = generalList.filter {
         color == COLOR_W
     }
-    val sumOfPerimeters = perimeter.sumOf { it.perimeter() }
-    val sumOfArea = area.sumOf { it.area() }
+    val sumOfPerimeters = perimeter.sum()
+    val sumOfArea = area.sum()
 
     println("Сумма периметров всех черных фигур: ${sumOfPerimeters.joinToString()}")
     println("Сумма площадей всех белых фигур: ${sumOfArea.joinToString()}")
@@ -35,26 +35,26 @@ abstract class Figure() {
 
 class Circle(
     color: String,
-    val radius: Int,
+    val radius: Double,
 ) : Figure() {
 
-    fun area(circle: Circle): Circle {
+    fun area(): Int {
         val square = NUMBER_PI * radius.toDouble().pow(SQUARING.toDouble())
         println(square)
-        return circle
+        return square
     }
 }
 
 class Rectangle(
     color: String,
-    val width: Int,
-    val height: Int,
+    val width: Double,
+    val height: Double,
 ) : Figure() {
 
-    fun perimeter(rectangle: Rectangle): Rectangle {
+    fun perimeter(): Double {
         val perimeter = (width + height) * CONSTANT_FOR_CALCULATING_PERIMETER
         println(perimeter)
-        return rectangle
+        return perimeter
     }
 }
 
