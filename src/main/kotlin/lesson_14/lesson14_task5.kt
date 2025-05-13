572abd247fe189b6200e0ccad602535ccbd23034
Sokolova <sokolovagb@gmail.com>
2025-05-13 21:19:08 +0300

    KS-14-5 start

--- src/main/kotlin/lesson_14/lesson14_task5.kt
+++ src/main/kotlin/lesson_14/lesson14_task5.kt
@@ -2,23 +2,21 @@ package org.example.lesson_14
 
 fun main() {
 
+    val message = Message()
+    val messageChild = ChildMessage()
+
 }
 
 open class Chat() {
     val chatMessage: MutableList<Chat> = mutableListOf()
 
     fun addMessage(id: Int, message: String) {
-       var parentMessageId: Int = 1
-        parentMessageId++
     }
 
     fun addThreadMessage(parentMessageId: Int, idChild: Int, messageChild: String) {
-
     }
 
     fun printChat(id: Int, idChild: Int, parentMessageId: Int) {
-        .groupBy(id)
-        .groupBy(parentMessageId)
     }
 }
 
@@ -26,12 +24,10 @@ class Message(
     val id: Int,
     val message: String,
     ) : Chat() {
-
 }
 
 class ChildMessage(
     val idChild: Int,
     val messageChild: String,
 ) : Chat() {
-
 }
