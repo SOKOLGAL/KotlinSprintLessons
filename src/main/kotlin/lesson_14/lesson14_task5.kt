67b7bd07dd59dfe4b043289909022718e1354e19
Sokolova <sokolovagb@gmail.com>
2025-05-15 11:29:47 +0300

    KS-14-5 question

--- src/main/kotlin/lesson_14/lesson14_task5.kt
+++ src/main/kotlin/lesson_14/lesson14_task5.kt
@@ -4,53 +4,27 @@ fun main() {
 
     val chat = Chat()
 
-    println("Для начала обсуждения введите ваше имя:")
-    val message1 = Message(
-        authorOfMessage = readln(),
-    )
-
-    println("${message1.authorOfMessage} введите сообщение:")
-    message1.message = readln()
+    println("Для начала обсуждения введите ваше имя и сообщение:")
     chat.addMessage(
-        authorOfMessage = message1.authorOfMessage,
-        message = message1.message
-    )
-
-    println("Для продолжения обсуждения ${message1.message} введите ваше имя:")
-    val messageChild1 = ChildMessage(
         authorOfMessage = readln(),
+        message = readln()
     )
 
-    println(
-        "${messageChild1.authorOfMessage} введите сообщение " +
-                "для продолжения обсуждения ${message1.message}:"
-    )
-    messageChild1.messageChild = readln()
+    println("Для продолжения обсуждения введите ваше сообщение:")
     chat.addThreadMessage(
-        messageChild = messageChild1.messageChild
+        parentMessageId = chat.parentMessageId,
+        messageChild = readln()
     )
-
-    println("Для продолжения обсуждения ${message1.message} введите ваше имя:")
-    val messageChild2 = ChildMessage(
-        authorOfMessage = readln(),
-    )
-
-    println(
-        "${messageChild2.authorOfMessage} введите сообщение " +
-                "для продолжения обсуждения ${message1.message}:"
-    )
-    messageChild2.messageChild = readln()
+    println("Для продолжения обсуждения введите ваше сообщение:")
     chat.addThreadMessage(
-        messageChild = messageChild2.messageChild
+        parentMessageId = chat.parentMessageId,
+        messageChild = readln()
     )
 
-    println(
-        "${messageChild2.authorOfMessage} введите сообщение " +
-                "для продолжения обсуждения ${message1.message}:"
-    )
-    messageChild2.messageChild = readln()
+    println("Для продолжения обсуждения введите ваше сообщение:")
     chat.addThreadMessage(
-        messageChild = messageChild2.messageChild
+        parentMessageId = chat.parentMessageId,
+        messageChild = readln()
     )
 
     chat.printChat()
@@ -60,21 +34,19 @@ fun main() {
 open class Chat() {
     var id = 0
     var parentMessageId = 0
-    val idChild = 0
-    val chatMessage: MutableList<Any> = mutableListOf()
-    val chatChildMessage: MutableList<Any> = mutableListOf()
+    val chatMessage: MutableList<Chat> = mutableListOf()
+    val chatChildMessage: MutableList<Chat> = mutableListOf()
+    var idChild = chatChildMessage.size + 1
 
-    fun addMessage(authorOfMessage: String, message: String): Int {
+    fun addMessage(authorOfMessage: String, message: String) {
         id++
         parentMessageId++
         chatMessage.add(id)
         chatMessage.add(authorOfMessage)
         chatMessage.add(message)
-        return parentMessageId
     }
 
-    fun addThreadMessage(messageChild: String) {
-        var idChild = 0
+    fun addThreadMessage(parentMessageId: Int, messageChild: String) {
         idChild++
         chatChildMessage.add(idChild)
         chatChildMessage.add(parentMessageId)
@@ -90,13 +62,11 @@ open class Message(
     val id: Int = 0,
     var authorOfMessage: String,
 ) {
-    var message: String = ""
 }
 
 class ChildMessage(
     id: Int = 0,
     authorOfMessage: String,
 ) : Message(id, authorOfMessage) {
-    val idChild: Int = 0
-    var messageChild: String = ""
+    val parentMessageId: Int = 0
 }
