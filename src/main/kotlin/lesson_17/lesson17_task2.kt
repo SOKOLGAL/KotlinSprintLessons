35910a66ac06732b28215c645481d7ccdf4baca9
Sokolova <sokolovagb@gmail.com>
2025-05-18 12:17:30 +0300

    KS-17-2 var name

---
+++ src/main/kotlin/lesson_17/lesson17_task2.kt
@@ -0,0 +1,24 @@
+package org.example.lesson_17
+
+fun main() {
+
+    val ship = Ship()
+    println("Название корабля: ${ship.name}")
+    ship.name = "Восточный берег"
+    println("Название корабля: ${ship.name}")
+
+}
+
+class Ship(
+) {
+    var name: String = "Победа"
+        get() = field
+        set(value: String) {
+            field != value
+            if (value != field) {
+                println("Предупреждение! Имя корабля менять нельзя!")
+            }
+        }
+    val averageSpeed = 50
+    val homePort: String = "Кронштад"
+}
