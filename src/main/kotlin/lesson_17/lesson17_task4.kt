228698a955e16f6a35e961657fa93c8aca49ef79
Sokolova <sokolovagb@gmail.com>
2025-05-19 22:03:55 +0300

    KS-17-2 movement counter

--- src/main/kotlin/lesson_17/lesson17_task4.kt
+++ src/main/kotlin/lesson_17/lesson17_task4.kt
@@ -4,12 +4,12 @@ fun main() {
 
     val parcel = Parcel(
         parcelNumber = 6768522700207,
-        currentLocation = "Пункт приема посылок"
+        currentLocation = "Пункт приема посылок ",
     )
 
-    parcel.movementCounter = "Пункт выдачи заказов"
+    parcel.newCurrentLocation = "Сортировочный терминал"
 
-    println("Посылка перемещена из ${parcel.currentLocation} в ${parcel.movementCounter}")
+    println("${parcel.movementCounter}")
 
 }
 
@@ -17,8 +17,12 @@ class Parcel(
     val parcelNumber: Long,
     var currentLocation: String,
 ) {
-    var movementCounter = "Сортировочный центр"
-        set(value: String) {
+    var newCurrentLocation = currentLocation
+        set(value) {
             field = value
+            if (newCurrentLocation != currentLocation) {
+                movementCounter++
+            }
         }
+    var movementCounter: Int = 1
 }
