e8c08b2663bd54c69783bfc119b69de7f20b7204
Sokolova <sokolovagb@gmail.com>
2025-05-26 16:41:36 +0300

    KS-20-1 string unit

---
+++ src/main/kotlin/lesson_20/lesson20_task1.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_20
+
+fun main() {
+
+    println("Введите ваше имя:")
+
+    val userName: String = readLine().toString()
+
+    val printStringWithLambda: (String) -> Unit
+
+    printStringWithLambda = {
+        println("С наступающим Новым Годом, $it")
+    }
+
+    printStringWithLambda(userName)
+}
