97df07ca28a29a0a6064bc59939950b4aab8aa48
Sokolova <sokolovagb@gmail.com>
2025-05-26 15:01:44 +0300

    KS-19-4 list

--- src/main/kotlin/lesson_19/lesson19_task4.kt
+++ src/main/kotlin/lesson_19/lesson19_task4.kt
@@ -23,31 +23,28 @@ enum class Cartridges(val impactForce: Int) {
 class Tank(
     val name: String,
     var cartridges: Int = 0,
-    var cartridgeTank: Int = 0,
 ) {
 
-    fun armingCartridges(cartridges: Cartridges): MutableList<Cartridges> {
+    fun armingCartridges(cartridges: Cartridges) {
         val listOfCartridges = mutableListOf(cartridges)
         when (cartridges) {
             Cartridges.BLUE -> listOfCartridges.add(Cartridges.BLUE)
             Cartridges.GREEN -> listOfCartridges.add(Cartridges.GREEN)
             Cartridges.RED -> listOfCartridges.add(Cartridges.RED)
         }
-        return listOfCartridges
     }
 
-    fun shooting(cartridges: Cartridges): MutableList<Cartridges> {
-        val listOfCartridges = mutableListOf(cartridges)
+    fun shooting(cartridges: Cartridges) {
+        val listOfShootingCartridges = mutableListOf(cartridges)
         when (cartridges) {
-            Cartridges.BLUE -> listOfCartridges.remove(Cartridges.BLUE)
-            Cartridges.GREEN -> listOfCartridges.remove(Cartridges.GREEN)
-            Cartridges.RED -> listOfCartridges.remove(Cartridges.RED)
+            Cartridges.BLUE -> listOfShootingCartridges.remove(Cartridges.BLUE)
+            Cartridges.GREEN -> listOfShootingCartridges.remove(Cartridges.GREEN)
+            Cartridges.RED -> listOfShootingCartridges.remove(Cartridges.RED)
         }
         when (cartridges) {
             Cartridges.BLUE -> println("Нанесенный урон ${Cartridges.BLUE.impactForce}")
             Cartridges.GREEN -> println("Нанесенный урон ${Cartridges.GREEN.impactForce}")
             Cartridges.RED -> println("Нанесенный урон ${Cartridges.RED.impactForce}")
         }
-        return listOfCartridges
     }
 }
