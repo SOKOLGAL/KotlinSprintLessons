3ee64a87271f06201dc8c417aa3cd14ca1c44f35
Sokolova <sokolovagb@gmail.com>
2025-05-26 22:16:05 +0300

    KS-20-4 lambda list

---
+++ src/main/kotlin/lesson_20/lesson20_task4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_20
+
+fun main() {
+
+    val listOfString: MutableList<String> = mutableListOf(
+        "Джек Воробей", "Гектор Барбаросса",
+        "Уилл Тёрнер", "Элизабет Суонн", "Джошими Гиббс"
+    )
+    val listOfLambda = listOfString.map { name ->
+        name.toList()
+        println("Нажат элемент: $name")
+    }
+
+    val filterIndexListOfLambda = listOfLambda.filterIndexed { index, s -> (index % 2 == 0) }
+    println(filterIndexListOfLambda)
+
+}
