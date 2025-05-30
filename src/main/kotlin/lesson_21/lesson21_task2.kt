e713fc92738182ec64b868dd32e94687305e5750
Sokolova <sokolovagb@gmail.com>
2025-05-30 14:25:08 +0300

    KS-21-2 this list of

--- src/main/kotlin/lesson_21/lesson21_task2.kt
+++ src/main/kotlin/lesson_21/lesson21_task2.kt
@@ -10,11 +10,9 @@ fun main() {
 }
 
 fun Int.evenNumbersSum() {
-    val int = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16)
-    val sumOfEvenNum = int.foldIndexed(0) { idx, sum, element ->
-        if ((idx % 2) == 0) {
-            sum + element
-        } else sum
-    }
-    println(sumOfEvenNum)
+    val int = listOf(this)
+    val sumOfEvenNum = int.filter {
+       it % 2 == 0
+        }
+    println(sumOfEvenNum.sum())
 }
