025f8602f03bfb679c245b15365db2fe83f7b165
Sokolova <sokolovagb@gmail.com>
2025-05-31 15:04:48 +0300

    KS-21-3 true false

---
+++ src/main/kotlin/lesson_21/lesson21_task3.kt
@@ -0,0 +1,26 @@
+package org.example.lesson_21
+
+fun main() {
+
+    val playerHealth = PlayerHealth(
+        name = "Джек",
+        currentHealth = 100,
+        maxHealth = 100
+    )
+
+    println(playerHealth.isHealthy())
+
+}
+
+class PlayerHealth(
+    val name: String,
+    var currentHealth: Int,
+    val maxHealth: Int,
+) {
+}
+
+fun PlayerHealth.isHealthy(): Boolean {
+    if (currentHealth == maxHealth) {
+        return true
+    } else {return false}
+}
