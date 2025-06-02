c201e3d517ac02aff558a2a8771c8d16eb6f6090
Sokolova <sokolovagb@gmail.com>
2025-06-02 19:46:49 +0300

    KS-21-5 max category

---
+++ src/main/kotlin/lesson_21/lesson21_task5.kt
@@ -0,0 +1,45 @@
+package org.example.lesson_21
+
+fun main() {
+
+    val character = Character("Ivolga")
+    character.runningSpeed()
+    character.jumpingHeight()
+    character.increasingStrength()
+    character.maxCategory()
+
+}
+
+class Character(
+    val name: String,
+    var experiment: Int = 0,
+    val key: String = "",
+    val value: Int = 100,
+) : MaxCategory {
+
+    override fun runningSpeed() {
+        val key = "Скорость бега"
+        val value = 100
+    }
+
+    override fun jumpingHeight() {
+        val key = "Высота прыжка"
+        val value = 75
+    }
+
+    override fun increasingStrength() {
+        val key = "Увеличение силы"
+        val value = 150
+    }
+}
+
+interface MaxCategory : Map<String, Int> {
+    fun runningSpeed()
+    fun jumpingHeight()
+    fun increasingStrength()
+}
+
+fun Character.maxCategory(): String {
+
+    return key
+}
