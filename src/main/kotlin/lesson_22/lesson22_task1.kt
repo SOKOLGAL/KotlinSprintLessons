9d18b9f10a5bdfed670565875656650576475633
Sokolova <sokolovagb@gmail.com>
2025-06-04 11:00:33 +0300

    KS-22-1 name of previous push

--- src/main/kotlin/lesson_22/lesson22_task1.kt
+++ src/main/kotlin/lesson_22/lesson22_task1.kt
@@ -9,6 +9,7 @@ fun main() {
 
     println(regularBook1 == regularBook2)
 //    TODO для class RegularBook сравниваются ссылки на объекты в памяти
+
     println(dataBook1 == dataBook2)
 //    TODO для data class DataBook сравниваются проинициализированные данные
 
