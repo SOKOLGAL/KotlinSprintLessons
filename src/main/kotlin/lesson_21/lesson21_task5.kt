34815a5b00a8ac9d09c43b73f111ae11aa0afc7c
Sokolova <sokolovagb@gmail.com>
2025-06-04 14:55:19 +0300

    KS-21-5 map

--- src/main/kotlin/lesson_21/lesson21_task5.kt
+++ src/main/kotlin/lesson_21/lesson21_task5.kt
@@ -2,46 +2,19 @@ package org.example.lesson_21
 
 fun main() {
 
-    val character = Character("Ivolga")
-    character.runningSpeed()
-    character.jumpingHeight()
-    character.increasingStrength()
-    character.maxCategory()
+    val experiencePoints = mapOf(
+        "Скорость бега" to 50,
+        "Высота прыжка" to 100,
+        "Бой" to 120,
+        "Магия" to 150,
+        "Выживание" to 150,
+        "Увеличение силы" to 75
+    )
+
+    println(experiencePoints.maxCategory())
 
 }
 
-class Character(
-    val name: String,
-    var experiment: Int = 0,
-    val key: String = "",
-    val value: Int = 0,
-) : Skills {
-
-    val listOfSkills = mutableListOf()
-
-    override fun runningSpeed() {
-        val key = "Скорость бега"
-        val value = 100
-    }
-
-    override fun jumpingHeight() {
-        val key = "Высота прыжка"
-        val value = 75
-    }
-
-    override fun increasingStrength() {
-        val key = "Увеличение силы"
-        val value = 150
-    }
-}
-
-interface Skills : Map<String, Int> {
-    fun runningSpeed()
-    fun jumpingHeight()
-    fun increasingStrength()
-}
-
-fun Character.maxCategory(): String {
-
-    return key
+fun Map<String, Int>.maxCategory(): String {
+    return this.maxBy { it.value }.key
 }
